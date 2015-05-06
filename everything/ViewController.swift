8866e8e9c11ab40ca438d57f81000410c7ecb945
ypwhs <915505626@qq.com>
2015-05-06 09:56:35 +0800

    Initial Commit

---
+++ everything/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  everything
+//
+//  Created by 杨培文 on 15/5/6.
+//  Copyright (c) 2015年 杨培文. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
